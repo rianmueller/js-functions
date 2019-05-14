4a289b101caa6fc31592b8d03b760b266b33bbc1
Rian Mueller <rian@MacBook-Pro.home>
2019-05-13 17:34:59 -1000

    exercise 7

--- exercises.js
+++ exercises.js
@@ -141,6 +141,11 @@ console.log(daDiva);
  * Console.log your results.
 */ 
 
+function checkDifference(x){
+    return '"Last night I dreamt that I ate ' + x + ' Taco Bell soft tacos."';
+}
+var difference = checkDifference(47);
+console.log(difference);
 
 /*
  * #8
